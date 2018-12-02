8b27ba832e9f4712a746e538ffe696f47b3700fe
Jarod Cajna <cajnajarod@gmail.com>
2018-12-02 09:39:02 +0100

    Update RetweetConcours.py

--- RetweetConcours.py
+++ RetweetConcours.py
@@ -2,15 +2,20 @@ import tweepy
 
 def retweet(api,NombreDeRetweet,listerecherchefr,tabname) :#Fonction de retweet de concours
     for mot in listerecherchefr : #Pour chaque mot dans la liste un lance une recherche
-        for tweet in tweepy.Cursor(api.search,q=mot,result_type="popular",lang="fr",tweet_mode="extended").items(NombreDeRetweet): #On cherche avec #concours parmis les plus populaires en france
+        for tweet in tweepy.Cursor(api.search,q=mot,lang="fr",tweet_mode="extended").items(NombreDeRetweet): #On cherche avec #concours parmis les plus populaires en france
             try:
-                tweet.retweet() #On retweet
-                api.create_friendship(tweet.user.id) #On follow
-                if "INVITER" in tweet.full_text.upper() : #On vérifie si il faut inviter des amies.
-                    commentaire(api,tweet,tabname)
-                elif "MENTIONNE" in tweet.full_text.upper() : #On vérifie si il faut inviter des amies.
-                    commentaire(api,tweet,tabname)
-                print('Vous avez retweet le tweet de  @' + tweet.user.screen_name)
+                if tweet.retweet_count > 5 :
+                    tweet.retweet() #On retweet
+                    if hasattr(tweet, 'retweeted_status') :
+                        api.create_friendship(tweet.retweeted_status.author.id)
+                        print('Vous avez retweet le tweet de  @' + tweet.retweeted_status.author.screen_name)
+                    else :
+                        api.create_friendship(tweet.user.id) #On follow
+                        print('Vous avez retweet le tweet de  @' + tweet.user.screen_name)
+                    if "INVITER" in tweet.full_text.upper() : #On vérifie si il faut inviter des amies.
+                        commentaire(api,tweet,tabname)
+                    elif "MENTIONNE" in tweet.full_text.upper() : #On vérifie si il faut inviter des amies.
+                        commentaire(api,tweet,tabname)
             except tweepy.TweepError as e:
                 print(e.reason)
             except StopIteration:
