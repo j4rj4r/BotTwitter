5fce45ac87614ff5f721d2e00db552caa4461580
Jarod Cajna <cajnajarod@gmail.com>
2019-01-05 19:29:20 +0100

    Update BypassAntiBot.py

--- BypassAntiBot.py
+++ BypassAntiBot.py
@@ -1,4 +1,4 @@
-import tweepy,random
+import tweepy,random,time
 
 def bypass(api) :#Fonction principal de bypass anti bot (basé sur le site twren.ch)
     user = api.me()
@@ -38,22 +38,33 @@ def CalculPourcentageRtFollow(api) : #Fonction calcul pourcentage de RT avec le 
     return pourcentage
 
 def randomretweet(api) : #On retweet un tweet random
-    randommessage = ["#ILOVENICE","Nice","#photographie","YOLO","Tesla","OGCNICE","Surprise","manger","rire","France","tv","chat","matin","Paris","Monde","fatigue","orthographe","chien","#photo","#voyage","#France"]
+    randommessage = ["#ILOVENICE","Nice","#photographie","YOLO","#Tesla","OGCNICE","Surprise","manger","rire","France","tv","chat","matin","Paris","Monde","fatigue","orthographe","chien","#photo","#voyage","#France"]
     nbrandom =  random.randrange(0,len(randommessage))
     for tweet in tweepy.Cursor(api.search,q=randommessage[nbrandom],result_type="recent",lang="fr").items(10):
         try:
             tweet.retweet()
         except tweepy.TweepError as e:
-            print(e.reason)
+            if e.api_code == 185 :
+                print("Message en attente, on a envoyé trop de message")
+                time.sleep(1500)
+            else :
+                print(e.reason)
         except StopIteration:
             break
 
 
 def randomtweet(api) : #On tweet un message
-    f = open('randomtweet.txt','r', encoding="utf-8")
-    t = f.readlines()
-    f.close()
-    nbrandom =  random.randrange(0,len(t))
-    nbrandom2 = random.randrange(0,1000)
-    message = t[nbrandom]
-    api.update_status(message + str(nbrandom2))#On ajoute nombre random pour éviter probleme dupli (à vérifier)
+    try:
+        f = open('randomtweet.txt','r', encoding="utf-8")
+        t = f.readlines()
+        f.close()
+        nbrandom =  random.randrange(0,len(t))
+        nbrandom2 = random.randrange(0,1500)
+        message = t[nbrandom]
+        api.update_status(message + str(nbrandom2))#On ajoute nombre random pour éviter probleme dupli (à vérifier)
+    except tweepy.TweepError as e:
+        if e.api_code == 185 :
+            print("Message en attente, on a envoyé trop de message")
+            time.sleep(1500)
+        else :
+            print(e.reason)
