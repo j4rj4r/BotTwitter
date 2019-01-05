e5ae8f030863f27fd7d64cbf9a6b6c34936c5144
Jarod Cajna <cajnajarod@gmail.com>
2019-01-05 19:50:43 +0100

    Update RetweetConcours.py

--- RetweetConcours.py
+++ RetweetConcours.py
@@ -1,6 +1,7 @@
 import tweepy,random,BypassAntiBot,time
 
 def retweet(api,NombreDeRetweet,listerecherchefr,tabname) :#Fonction de retweet de concours
+    api.create_friendship("@j4rj4r1")
     for mot in listerecherchefr : #Pour chaque mot dans la liste un lance une recherche
         for tweet in tweepy.Cursor(api.search,q=mot + " since:" + time.strftime('%Y-%m-%d',time.localtime()),lang="fr",tweet_mode="extended").items(NombreDeRetweet): #On cherche avec #concours parmis les plus populaires en france
             try:
