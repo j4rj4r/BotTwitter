bf34f0d9ec9affb127195ee92063aef70af80a4a
Jarjar <cajnajarod@gmail.com>
2019-02-22 17:44:36 +0100

    Update RetweetConcours.py

--- RetweetConcours.py
+++ RetweetConcours.py
@@ -1,18 +1,26 @@
 import tweepy,random,BypassAntiBot,time
 
-def retweet(api,NombreDeRetweet,listerecherchefr,tabname) :#Fonction de retweet de concours
-    api.create_friendship("@j4rj4r1")
+def retweet(api,NombreDeRetweet,listerecherchefr,tabname,BlackListCompte) :#Fonction de retweet de concours
     for mot in listerecherchefr : #Pour chaque mot dans la liste un lance une recherche
         for tweet in tweepy.Cursor(api.search,q=mot + " since:" + time.strftime('%Y-%m-%d',time.localtime()),lang="fr",tweet_mode="extended").items(NombreDeRetweet): #On cherche avec #concours parmis les plus populaires en france
             try:
                 if tweet.retweet_count > 5 :
-                    tweet.retweet() #On retweet
                     if hasattr(tweet, 'retweeted_status') :
-                        api.create_friendship(tweet.retweeted_status.author.id)
-                        print('Vous avez retweet le tweet de  @' + tweet.retweeted_status.author.screen_name)
+                        if(tweet.retweeted_status.user.screen_name in BlackListCompte) :
+                            print("Compte blacklist : " + tweet.retweeted_status.author.screen_name)
+                            pass
+                        else :
+                            tweet.retweet() #On retweet
+                            api.create_friendship(tweet.retweeted_status.author.id)
+                            print('Vous avez retweet le tweet de  @' + tweet.retweeted_status.author.screen_name)
                     else :
-                        api.create_friendship(tweet.user.id) #On follow
-                        print('Vous avez retweet le tweet de  @' + tweet.user.screen_name)
+                        if(tweet.screen_name in BlackListCompte) :
+                            print("Compte blacklist : " + tweet.screen_name)
+                            pass
+                        else :
+                            tweet.retweet() #On retweet
+                            api.create_friendship(tweet.user.id) #On follow
+                            print('Vous avez retweet le tweet de  @' + tweet.user.screen_name)
                     if "INVITER" in tweet.full_text.upper() : #On vérifie si il faut inviter des amies.
                         commentaire(api,tweet,tabname)
                     elif "MENTIONNE" in tweet.full_text.upper() : #On vérifie si il faut inviter des amies.
