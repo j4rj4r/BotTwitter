71829d31792849742d4ab08b7b43095e3ea82d52
Jarjar <cajnajarod@gmail.com>
2019-05-11 12:56:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-import tweepy,RetweetConcours,BypassAntiBot,time,random,sys,GestionFollow
+import tweepy,RetweetConcours,BypassAntiBot,time,random,sys,GestionFollow,csv
 tabname = []
 
 ###Constante Paramètre du bot ###
 version = 2.8 #Version du bot
-compte = {"1":["","","",""],"2":["","","",""],"3":["","","",""],"4":["","","",""]} #Liste des comptes avec les identifiants de connexion à l'api
+compte = {"1":["","","",""],"2":["","","",""]} #Liste des comptes avec les identifiants de connexion à l'api
 NombreDeRetweet = 12 #Nombre de tweet que l'on recupère par recherche
 listerecherchefr = ["#concours","#JeuConcours","RT & Follow","tenter de gagner","Gagnez rt + follow","concours pour gagner"]#Mot à retweeté pour un concours
-BlackListCompte = ["NistikConcours","WqveConcours","FlawyxC","Linyz_V1","FortniteVenox","TidaGameuse","YeastLeaks","CrashqConcours","Yanteh_","NistiKTV",]#Blacklist pour compte à concours (très) bidon | Il faut metre le pseudo après le @
+BlackListCompte = ["gulzaarAvi","NistikConcours","WqveConcours","FlawyxC","Linyz_V1","FortniteVenox","TidaGameuse","YeastLeaks","CrashqConcours","Yanteh_","NistiKTV",]#Blacklist pour compte à concours (très) bidon | Il faut metre le pseudo après le @
 CompteTag = ["@j4rj4r_binks"]#Les comptes à utiliser pour tag. Si vous utilisez plusieurs comptes bot vous n'avez pas besoins d'ajouter de comptes dans ce tableau. Vous devez rentrer le compte avec son @ (@toto)
 ###
+
 
 for cle,tabauth in compte.items():
     try :
@@ -25,6 +26,7 @@ for cle,tabauth in compte.items():
             print(e.reason)
 tabname = tabname + CompteTag
 
+
 while True :
     for tabauth in compte.values(): #Pour chaque compte on passe dans cette boucle
         try :
@@ -33,7 +35,7 @@ while True :
             api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True) #Authentification
             user = api.me()
             GestionFollow.Unfollow(user,api)
-            RetweetConcours.retweet(api,NombreDeRetweet,listerecherchefr,tabname,BlackListCompte,CompteTag)#on retweet les concours
+            RetweetConcours.retweet(api,NombreDeRetweet,listerecherchefr,tabname,BlackListCompte)#on retweet les concours
             BypassAntiBot.bypass(api)#On bypass l'anti bot
         except tweepy.TweepError as e :
             if e.api_code == 326 :
