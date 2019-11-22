da6a1c0426c3a18cbf27f294a0c6141b8337a583
Jarjar <cajnajarod@gmail.com>
2019-11-22 20:40:27 +0100

    Update BypassAntiBot.py

--- BypassAntiBot.py
+++ BypassAntiBot.py
@@ -80,7 +80,7 @@ def randomtweet(api) : #On récupère un message tweeter et on le tweet
                 if "#" in tweettext : #On évite de notifier les gens quand on récupère un tweet d'un autre
                     tweettext = tweettext.replace("#"," ")
                 api.update_status(tweettext)
-                time.sleep(10)
+                time.sleep(20)
     except tweepy.TweepError as e:
         if e.api_code == 185 :
             print("Message en attente, on a envoyé trop de message")
