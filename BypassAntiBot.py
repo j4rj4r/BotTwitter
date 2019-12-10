4c3da3bee46e6c37c75a19bf806083b843f707da
Jarjar <cajnajarod@gmail.com>
2019-12-10 15:18:47 +0100

    Update BypassAntiBot.py

--- BypassAntiBot.py
+++ BypassAntiBot.py
@@ -5,97 +5,110 @@ import time
 #Bibliothèques tierces
 import tweepy
 
-
-def bypass(api) :#Fonction principal de bypass anti bot (basé sur le site twren.ch)
-    try :
+#Fonction principal de bypass anti bot (basé sur le site twren.ch)
+def bypass(api):
+    try:
         print("Bypass des protections Pickaw en cours")
         nb = 1
         pourcentageRTFollow = CalculPourcentageRtFollow(api)
-        while pourcentageRTFollow >= 25 : #On veut moins de 25% de RT avec le mot Follow
+        #On veut moins de 25% de RT avec le mot Follow
+        while pourcentageRTFollow >= 25:
             randomretweet(api)
             pourcentageRTFollow = CalculPourcentageRtFollow(api)
             print("Pourcentage de Rt avec le mot Follow (en cours de diminution) : " + str(round(pourcentageRTFollow, 2)))
         print("Pourcentage de Rt avec le mot Follow (final) : " + str(round(pourcentageRTFollow, 2)))
         pourcentageRT = CalculPourcentageRT(api)
-        while pourcentageRT >= 50 : #On veut moins de 50% de RT
+        #On veut moins de 50% de RT
+        while pourcentageRT >= 50:
             randomtweet(api)
             pourcentageRT = CalculPourcentageRT(api)
             nb += 1
         print("Pourcentage RT final : " + str(round(pourcentageRT, 2)))
-    except tweepy.TweepError as e :
-        if e.api_code == 326 :
+    except tweepy.TweepError as e:
+        if e.api_code == 326:
             pass
 
-def CalculPourcentageRT(api) : #Fonction calcul pourcentage RT  de l'utilisateur
+#Fonction calcul pourcentage RT  de l'utilisateur
+def CalculPourcentageRT(api):
     nb = 0
-    for tweet in api.user_timeline(count=200, tweet_mode="extended") :
-        if tweet.retweeted == True :#Si le tweet est un RT
+    for tweet in api.user_timeline(count=200, tweet_mode="extended"):
+        #Si le tweet est un RT
+        if tweet.retweeted:
             nb += 1
-    pourcentage = (nb * 100)/200 #Calcul pourcentage
+    #Calcul pourcentage
+    pourcentage = (nb * 100)/200
     return pourcentage
 
-def CalculPourcentageRtFollow(api) : #Fonction calcul pourcentage de RT avec le mot Follow
+#Fonction calcul pourcentage de RT avec le mot Follow
+def CalculPourcentageRtFollow(api):
     nb = 0
     nbrt = 0
-    for tweet in api.user_timeline(count=200, tweet_mode="extended") :
-        if tweet.retweeted == True :
+    for tweet in api.user_timeline(count=200, tweet_mode="extended"):
+        if tweet.retweeted:
             nbrt += 1
-            if "FOLLOW" in tweet.full_text.upper() : #On met tout le texte en majuscule et n cherche le mot follow dans le tweet
+            if "FOLLOW" in tweet.full_text.upper(): #On met tout le texte en majuscule et n cherche le mot follow dans le tweet
                 nb += 1
     pourcentage = (nb * 100)/nbrt
     return pourcentage
 
-def randomretweet(api) : #On retweet un tweet random
+def randomretweet(api): #On retweet un tweet random
     trends1 = api.trends_place(610264)
     trends = list([trend['name'] for trend in trends1[0]['trends']])
-    nbrandom =  random.randrange(0,len(trends))
-    for tweet in tweepy.Cursor(api.search,q=trends[nbrandom],result_type="recent",lang="fr").items(10):
+    nbrandom = random.randrange(0, len(trends))
+    for tweet in tweepy.Cursor(api.search, q=trends[nbrandom], result_type="recent", lang="fr").items(10):
         try:
             tweet.retweet()
         except tweepy.TweepError as e:
-            if e.api_code == 185 :
+            if e.api_code == 185:
                 print("Message en attente, on a envoyé trop de message")
                 time.sleep(1500)
-            elif (e.api_code == 327) or (e.api_code == 326) :
+            elif (e.api_code == 327) or (e.api_code == 326):
                 pass
-            else :
+            else:
                 print(e.reason)
         except StopIteration:
             break
 
-
-def randomtweet(api) : #On récupère un message twitter et on le tweet
+#On récupère un message twitter et on le tweet
+def randomtweet(api):
     try:
-        trends1 = api.trends_place(610264)#Code France (marseille) FR
-        trends = list([trend['name'] for trend in trends1[0]['trends']]) #On récupère la liste des tendances
-        nbrandom =  random.randrange(0,len(trends))
-        for tweet in tweepy.Cursor(api.search,q=trends[nbrandom] + " -filter:replies -filter:media -filter:retweets",lang="fr",tweet_mode="extended",result_type='recent').items(1):
-            if hasattr(tweet, 'retweeted_status') :
-                if "CONCOURS" in tweet.retweeted_status.full_text.upper() : #On ne veut pas tweet un concours
+        #Code France (marseille) FR
+        trends1 = api.trends_place(610264)
+        #On récupère la liste des tendances
+        trends = list([trend['name'] for trend in trends1[0]['trends']])
+        nbrandom = random.randrange(0, len(trends))
+        #On cherche des tweets parmis les tweets recents
+        for tweet in tweepy.Cursor(api.search, q=trends[nbrandom] + " -filter:replies -filter:media -filter:retweets", lang="fr", tweet_mode="extended", result_type='recent').items(1):
+            if hasattr(tweet, 'retweeted_status'):
+                #On ne veut pas tweet un concours
+                if "CONCOURS" in tweet.retweeted_status.full_text.upper():
                     pass
-                else :
+                else:
                     tweettext = tweet.retweeted_status.full_text
-                    if "@" in tweettext : #On évite de notifier les gens quand on récupère un tweet d'un autre
-                        tweettext = tweettext.replace("@"," ")
-                    if "#" in tweettext : #On évite les # pour etre discret
-                        tweettext = tweettext.replace("#"," ")
+                    #On évite de notifier les gens quand on récupère un tweet d'un autre
+                    if "@" in tweettext:
+                        tweettext = tweettext.replace("@", " ")
+                    #On évite les # pour etre discret
+                    if "#" in tweettext:
+                        tweettext = tweettext.replace("#", " ")
                     api.update_status(tweettext)
-            else :
-                if "CONCOURS" in tweet.full_text.upper() : #On ne veut pas tweet un concours
+            else:
+                #On ne veut pas tweet un concours
+                if "CONCOURS" in tweet.full_text.upper():
                     pass
                 else:
                     tweettext = tweet.full_text
-                    if "@" in tweettext :
-                        tweettext = tweettext.replace("@"," ")
-                    if "#" in tweettext : #On évite de notifier les gens quand on récupère un tweet d'un autre
-                        tweettext = tweettext.replace("#"," ")
+                    if "@" in tweettext:
+                        tweettext = tweettext.replace("@", " ")
+                    if "#" in tweettext:
+                        tweettext = tweettext.replace("#", " ")
                     api.update_status(tweettext)
             time.sleep(20)
     except tweepy.TweepError as e:
-        if e.api_code == 185 :
+        if e.api_code == 185:
             print("Message en attente, on a envoyé trop de message")
             time.sleep(1500)
         elif (e.api_code == 187) or (e.api_code == 327) or (e.api_code == 186) or (e.api_code == 326):
             pass
-        else :
+        else:
             print(e.reason)
